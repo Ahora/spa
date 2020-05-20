ccd92bc9e83241849db465fe80423c0441ad054e
Erez Alster <erez@alster.co.il>
2020-05-20 10:56:32 +0300

    ds

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ echo "Downloading $SPAARCHIVEURL"
 curl $SPAARCHIVEURL --silent --output archive.tar.gz
 [ $? -eq 0 ]  || exit 1
 echo "extracting $SPAARCHIVEURL"
-tar -C /usr/share/nginx/html -zxf archive.tar.gz
+tar -C /usr/share/nginx/html -xzvf archive.tar.gz
 [ $? -eq 0 ]  || exit 1
 echo "$SPAARCHIVEURL extracted successfully"
 rm archive.tar.gz
