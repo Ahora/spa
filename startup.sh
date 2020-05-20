b99ea6947aa0d446b01691379bb9a11717ada785
Erez Alster <erez@alster.co.il>
2020-05-20 10:38:34 +0300

    print errors

--- startup.sh
+++ startup.sh
@@ -5,6 +5,8 @@ curl $SPAARCHIVEURL --silent --output archive.tar.gz
 [ $? -eq 0 ]  || exit 1
 echo "extracting $SPAARCHIVEURL"
 tar -C /usr/share/nginx/html -zxf archive.tar.gz
+[ $? -eq 0 ]  || exit 1
+echo "$SPAARCHIVEURL extracted successfully"
 rm archive.tar.gz
 echo "Starting nginx"
 nginx -g 'daemon off;'
